017716c3df09e81b78575562be0099485ec42c67
abidurrahamanjoy <abidurrahmanjoy56@gmail.com>
2026-09-21 11:43:46 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Offline cache. অ্যাপ আপডেট করলে VERSION বদলে দিন।
+const VERSION = "joy-scanner-v2";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(VERSION).then((c) => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== VERSION).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request).catch(() => caches.match("./index.html")))
+  );
+});
